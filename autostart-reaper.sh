661d795bcc21db1b3a86a512955118ea3db26cde
Mr. Sorensen <mrsorensen@protonmail.com>
2019-11-15 10:35:08 +0100

    Bugfix

--- autostart-reaper.sh
+++ autostart-reaper.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source ~/colors.sh
+
+
 # Keep going
 while :
 do
@@ -8,20 +11,22 @@ do
 	# Runs reaper if BEHRINGER device is connected
 	# and reaper is not already running
 	if [[ $usbresult ]]; then
-		echo "Found device"
+		printf "${GREEN}Found device${NC}\n"
 		# Returns whether reaper is running or not
 		reaperId=$(pgrep -x reaper)
 		# Run reaper if reaper is not already running
 		if [[ -e "$reaperId" || "$reaperId" == "" ]]; then
-			echo "Reaper is not running. Starting now..."
+			printf "${RED}Reaper{NC} is not running. ${GREEN}Starting now...${NC}\n"
 			nohup reaper &
 		else
 			echo "Reaper is running"
 		fi
 	else
-		echo "USB disconnected. Trying to kill reaper"
-		killall reaper
+		printf "\r${RED}USB device not connected. ${NC}"
+		if [[ $(pgrep reaper | wc -l) == 2 ]]; then
+			printf "Killing ${PURPLE}REAPER${NC}"
+			killall reaper
+		fi
 	fi
 	sleep 2
-	echo "..."
 done
